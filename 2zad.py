eb09ee8e0897c062d89ba115b37da1f8187edfb1
Arsen445 <100753942+Arsen445@users.noreply.github.com>
2022-10-18 11:40:19 +0300

    Update 2zad.py

--- 2zad.py
+++ 2zad.py
@@ -4,29 +4,26 @@ import sys
 from datetime import date
 import math
 
-
 cir = 1
-def cylinder():
-    
+
+def cylinder():    
     r = float(input("введите радиус "))
     h = float(input("введите высоту "))
     
     def circle(r):
-        r = 3.14*(r**2)
+        r = 3.14 * (r ** 2)
         global cir
         cir = r
       
-
     print("S: Полная-1  Боковая-2")
-    d=int(input())
+    d = int(input())
    
-    if d==1:
-        print((2*3.14*r)+h+(2*cir))
-    if d==2:
-        print((2*3.14*r)+h)
+    if d == 1:
+        print((2 * 3.14 * r) + h + (2 * cir))
+    if d == 2:
+        print((2 * 3.14 * r) + h)
 
         
 if __name__ == '__main__':
-   
-    (cylinder())
+    cylinder()
     
