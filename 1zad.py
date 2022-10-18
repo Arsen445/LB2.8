3e87524ae6f4acd0c7fd663ced534f88be17250b
Arsen445 <100753942+Arsen445@users.noreply.github.com>
2022-10-18 11:37:55 +0300

    Update 1zad.py

--- 1zad.py
+++ 1zad.py
@@ -3,24 +3,21 @@
 import sys
 from datetime import date
 
-
-
-
 def test():
     x = int(input())
-    if x>=0:
+    if x >= 0:
         positive()
     else:
         negative()
-
+        
 
 def positive():
     print("положительное")
 
+    
 def negative():
     print("Отрицательное") 
     
         
-
 if __name__ == '__main__':
     test()
